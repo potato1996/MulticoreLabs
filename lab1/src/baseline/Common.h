dbed4584909d6fd5f149a42f059a536644d300de
Dayou Du <dudayou1901@gmail.com>
2018-03-06 14:57:44 -0500

    upload lab1

---
+++ lab1/src/baseline/Common.h
@@ -0,0 +1,7 @@
+#pragma once
+#define testBit(a,i) ((a) & (1 << i))
+#define setBit(a,i) ((a) | (1 << i))
+#define toOneDim(i,j,n) ((i)*(n)+(j))
+#define MAXCITIES 10
+#define DEBUG
+#define USE_MULTI_THREAD
---
+++ lab1/src/version1/MultithreadSolution.cpp
@@ -0,0 +1,124 @@
+#include "MultithreadSolution.h"
+#include "Common.h"
+#include<cstdlib>
+#include<cstdio>
+#include<climits>
+#include<omp.h>
+namespace {
+	void DFS(const int* dis,
+		const int numCities,
+		const int currDis,
+		const int currCity,
+		int* currAnsSeq,
+		const int mask,
+		const int depth,
+		int& minDis_s,
+		int& minDis,
+		int* minAnsSeq) {
+		if (depth == numCities) {
+			bool setSeq = false;
+#pragma omp critical
+			{
+				if (currDis < minDis_s) {
+					minDis_s = currDis;
+					setSeq = true;
+				}
+			}
+			if (setSeq) {
+				minDis = currDis;
+				for (int i = 0; i < numCities; ++i) {
+					minAnsSeq[i] = currAnsSeq[i];
+				}
+			}
+			return;
+		}
+		for (int i = 0; i < numCities; ++i) {
+			if (testBit(mask, i))continue;
+			const int disi = dis[toOneDim(currCity, i, numCities)];
+
+			int globalMinDis;
+#pragma omp atomic read
+			globalMinDis = minDis_s;
+
+			if (currDis + disi >= globalMinDis)continue;
+			currAnsSeq[depth] = i;
+			DFS(dis, numCities,
+				currDis + disi,
+				i,
+				currAnsSeq,
+				setBit(mask, i),
+				depth + 1,
+				minDis_s,
+				minDis,
+				minAnsSeq);
+		}
+	}
+}//anonymous namespace
+void MultithreadSolution::tsm(const int* dis, 
+	const int numCities, 
+	const int numThreads)
+{
+	omp_set_num_threads(numThreads);
+
+	//shared variables
+	int minDis_s = INT_MAX;
+	int *minDis_ns = new int[numThreads];
+	int *minAnsSeqs = new int[numThreads * numCities];
+
+#pragma omp parallel
+	{
+	//private variables
+		int tid = omp_get_thread_num();
+		int currAnsSeq[MAXCITIES];
+		int *minAnsSeq = minAnsSeqs + tid * numCities;
+
+		minDis_ns[tid] = INT_MAX;
+
+#pragma omp for schedule(static)
+		for (int i = 0; i < numCities; ++i) {
+			currAnsSeq[0] = i;
+			DFS(dis, numCities,
+				0, i, currAnsSeq,
+				setBit(0, i), 1, minDis_s, minDis_ns[tid], minAnsSeq);
+		}
+	}
+	//get the result
+	for (int i = 0; i < numThreads; ++i) {
+		if (minDis_ns[i] == minDis_s) {
+			int* minAnsSeq = minAnsSeqs + i * numCities;
+
+#ifdef DEBUG
+			int checkCount = 0;
+#endif
+
+			printf("Best path:");
+			for (int j = 0; j < numCities; ++j) {
+				printf(" %d", minAnsSeq[j]);
+
+#ifdef DEBUG
+				if(j > 0){
+					checkCount += dis[toOneDim(minAnsSeq[j-1],minAnsSeq[j],numCities)];
+				}
+#endif
+
+			}
+			printf("\nDistance: %d\n", minDis_s);
+
+#ifdef DEBUG
+			if(checkCount == minDis_s){
+				printf("Ans Check Pass\n");
+			}
+			else{
+				printf("Ans Check FAILED\n");
+			}
+#endif
+
+			break;
+		}
+	}
+
+	//clean up
+	delete minDis_ns;
+	delete minAnsSeqs;
+
+}
