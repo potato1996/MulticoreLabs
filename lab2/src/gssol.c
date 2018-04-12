dec88c5a797c5d320eb6587a4706bc0c659c0003
Dayou Du <dudayou1901@gmail.com>
2018-04-12 17:46:09 -0400

    use double for updating

--- lab2/src/gssol.c
+++ lab2/src/gssol.c
@@ -16,14 +16,16 @@ int solve_one_iter(const data_float_t* restrict a_local,
 	int unfinish = 0;
 	for(int i = 0; i < proc_chunksize; ++i){
 		const int curr_row = i + proc_startid;
-		data_float_t x_i_new = b_local[i] + a_local[TO_ONE_DIM(i, curr_row, num)] * x[curr_row];
+		//NOTE: HERE WE FORCE TO USE DOUBLE DURING UPDATING
+		//AVOID PRECISION PROBLEM
+		double x_i_new = (double)b_local[i] + a_local[TO_ONE_DIM(i, curr_row, num)] * x[curr_row];
 		for(int j = 0; j < num; ++j){
 			x_i_new -= a_local[TO_ONE_DIM(i, j, num)] * x[j];
 		}
-		//x_i_new += a_local[TO_ONE_DIM(i, curr_row, num)] * x[curr_row];
-		x_new[curr_row] = x_i_new / a_local[TO_ONE_DIM(i, curr_row, num)];
-		data_float_t delta = (x_new[curr_row] - x[curr_row])/(x_new[curr_row]);
+		x_i_new = x_i_new / a_local[TO_ONE_DIM(i, curr_row, num)];
+		double delta = (x_i_new - x[curr_row])/(x_new[curr_row]);
 		unfinish |= ((fabs(delta) >= err) ? 1 : 0);
+		x_new[curr_row] = (float)x_i_new;
 #ifdef DEBUG
 		printf("x_%d, old val = "FORMAT_STR", new val = "FORMAT_STR", delta = "FORMAT_STR", unfinish = %d\n",
 				curr_row, x[curr_row], x_new[curr_row], delta, unfinish);
